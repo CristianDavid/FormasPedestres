e921914c789faac1cb8abc6f3825feb11cd23103
Cristian David González González <cristian_david_g@live.com>
2017-05-10 10:37:51 -0500

    Ruta de libsvm corregida

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #DIR=src/
 #descomentar si las carpetas de imágenes están donde mismo que el código
 DIR=./
-export PYTHONPATH=$PYTHONPATH:"${DIR}libsvm-3.22/libsvm-3.22/python/"
+export PYTHONPATH=$PYTHONPATH:"${DIR}libsvm-3.22/python/"
 #python "${DIR}descriptor.py"
 python "${DIR}descriptorwui.py"
