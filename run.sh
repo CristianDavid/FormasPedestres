24a724983a2d269bb0c4e94c45c1bda7e624bf11
Cristian David González González <cristian_david_g@live.com>
2017-05-10 10:47:00 -0500

    More flexible running script

--- run.sh
+++ run.sh
@@ -1,6 +1,5 @@
+#!/usr/bin/env sh
 #DIR=src/
-#descomentar si las carpetas de imágenes están donde mismo que el código
 DIR=./
 export PYTHONPATH=$PYTHONPATH:"${DIR}libsvm-3.22/python/"
-#python "${DIR}descriptor.py"
-python "${DIR}descriptorwui.py"
+python $1
